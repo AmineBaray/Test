6156065aaeea065c684e25e62fb1e27daa140bfb
AmineBaray <barayamine101@gmail.com>
2024-05-21 01:48:17 +0100

    add html css to the file

--- test.php
+++ test.php
@@ -1,2 +1,18 @@
-<?php
-echo "hello im in internship";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        h1{
+            color:blue;
+        }
+    </style>
+</head>
+<body>
+    <?php
+        echo "<h1>Laedx internship! </h1>"
+    ?>
+</body>
+</html>
