4bcd9878a24206ad66d51304ea2609dafbfdd988
MustaphaJAM <mustapha.mj.825@gmail.com>
2024-06-22 14:20:36 +0100

    fix lougout issue

--- app/components/elementsUi/Popup.tsx
+++ app/components/elementsUi/Popup.tsx
@@ -93,19 +93,44 @@ const Popup: React.FC<PopupProps> = ({ onClose, profileImage }) => {
     setShowConfirmation(false);
   };
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-      <div className="bg-white p-3 rounded-lg shadow-lg w-auto relative">
-        <button className="absolute top-2 right-2 text-black text-2xl" onClick={onClose}>x</button>
+  const handleOverlayClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    if (event.target === event.currentTarget) {
+      onClose();
+    }
+  };
 
-        <div className="flex flex-row">
-          <div className="flex flex-col mx-8 mt-4">
-            <img src={imageSrc || '/images/placeholder.jpg'} alt="Profile Image" className="rounded-full w-24 h-24 object-cover" />
-            <button className="hover:border-gray-300 hover:border-2 text-red-700 px-4 py-2 rounded-lg font-bold mt-2" onClick={handleDelete}>delete</button>
+  return (
+    <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={handleOverlayClick}>
+      <div className="bg-white p-3 rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 relative">
+        <button
+          className="absolute top-2 right-2 text-black text-2xl"
+          onClick={onClose}
+        >
+          x
+        </button>
+
+        <div className="flex flex-col md:flex-row">
+          <div className="flex flex-col items-center mx-8 mt-4">
+            <div className="w-24 h-24">
+              <img
+                src={imageSrc || '/images/placeholder.jpg'}
+                alt="Profile Image"
+                className="rounded-full w-full h-full object-cover"
+              />
+            </div>
+            <button
+              className="text-red-700 px-4 py-2 rounded-lg font-bold mt-2 border-2 border-transparent hover:border-gray-300"
+              onClick={handleDelete}
+            >
+              delete
+            </button>
           </div>
-          <div className="flex flex-col gap-8">
-            <p className="text-zinc-700 mt-4">Select an image to upload</p>
-            <div className="custom-file-upload">
+
+          <div className="flex flex-col gap-4 md:gap-8 w-full mt-4 md:mt-0">
+            <p className="text-zinc-700 text-center md:text-left">
+              Select an image to upload
+            </p>
+            <div className="custom-file-upload text-center md:text-left">
               <input
                 type="file"
                 id="fileInput"
@@ -126,7 +151,7 @@ const Popup: React.FC<PopupProps> = ({ onClose, profileImage }) => {
                 {successMessage}
               </div>
             )}
-            <div className="flex space-x-2 justify-end">
+            <div className="flex space-x-2 justify-center md:justify-end mt-4">
               <button
                 className="border-pink-500 border-2 text-pink-500 hover:bg-pink-500 hover:text-white px-4 py-2 rounded-lg"
                 onClick={handleCancel}
@@ -134,7 +159,7 @@ const Popup: React.FC<PopupProps> = ({ onClose, profileImage }) => {
                 cancel
               </button>
               <button
-                className="bg-pink-500 text-white px-4 py-2 rounded-lg"
+                className="bg-pink-500 hover:bg-pink-300 text-white px-4 py-2 rounded-lg"
                 onClick={handleSave}
               >
                 save
@@ -144,9 +169,11 @@ const Popup: React.FC<PopupProps> = ({ onClose, profileImage }) => {
         </div>
 
         {showConfirmation && (
-          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
-            <div className="bg-white p-3 rounded-lg shadow-lg w-auto relative">
-              <p className="text-center mb-4">Are you sure you want to delete this image?</p>
+          <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50" onClick={handleOverlayClick}>
+            <div className="bg-white p-3 rounded-lg shadow-lg w-11/12 md:w-2/3 lg:w-1/2 xl:w-1/3 relative">
+              <p className="text-center mb-4">
+                Are you sure you want to delete this image?
+              </p>
               <div className="flex justify-center space-x-4">
                 <button
                   className="bg-red-500 text-white px-4 py-2 rounded-lg"
